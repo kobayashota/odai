45f245f6ae55a492bcdb1102f01990c43de14ad2
kobashou06 <kobashou06@gmail.com>
2021-08-19 13:11:04 +0900

    add Pods, UIViewControllers

---
+++ sampleFireStore/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  sampleFireStore
+//
+//  Created by kobashou06 on 2021/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topStackView: UIStackView!
+    @IBOutlet weak var bottomStackView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
